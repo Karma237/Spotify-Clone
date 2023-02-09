6b3921e103a200fba3ae8980edc54080a0505fa2
Karma237 <77337048+Karma237@users.noreply.github.com>
2023-02-09 20:24:25 +0200

    Add files via upload

---
+++ pro/mainForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace pro
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.songsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.browseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.byCategoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bySongIDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.likeSongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.artistsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchSongsForSpecificToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.myProfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewLibraryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageAccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editPackageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.SpringGreen;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.songsToolStripMenuItem,
+            this.artistsToolStripMenuItem,
+            this.myProfileToolStripMenuItem,
+            this.manageAccountToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(660, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // songsToolStripMenuItem
+            // 
+            this.songsToolStripMenuItem.BackColor = System.Drawing.Color.PaleGreen;
+            this.songsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.browseToolStripMenuItem,
+            this.likeSongToolStripMenuItem});
+            this.songsToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.songsToolStripMenuItem.Name = "songsToolStripMenuItem";
+            this.songsToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.songsToolStripMenuItem.Text = "Songs";
+            // 
+            // browseToolStripMenuItem
+            // 
+            this.browseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.byCategoryToolStripMenuItem,
+            this.bySongIDToolStripMenuItem});
+            this.browseToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseToolStripMenuItem.Name = "browseToolStripMenuItem";
+            this.browseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.browseToolStripMenuItem.Text = "Browse";
+            // 
+            // byCategoryToolStripMenuItem
+            // 
+            this.byCategoryToolStripMenuItem.Name = "byCategoryToolStripMenuItem";
+            this.byCategoryToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.byCategoryToolStripMenuItem.Text = "By category";
+            this.byCategoryToolStripMenuItem.Click += new System.EventHandler(this.byCategoryToolStripMenuItem_Click);
+            // 
+            // bySongIDToolStripMenuItem
+            // 
+            this.bySongIDToolStripMenuItem.Name = "bySongIDToolStripMenuItem";
+            this.bySongIDToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bySongIDToolStripMenuItem.Text = "By Song ID";
+            this.bySongIDToolStripMenuItem.Click += new System.EventHandler(this.bySongIDToolStripMenuItem_Click);
+            // 
+            // likeSongToolStripMenuItem
+            // 
+            this.likeSongToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.likeSongToolStripMenuItem.Name = "likeSongToolStripMenuItem";
+            this.likeSongToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.likeSongToolStripMenuItem.Text = "Like song";
+            this.likeSongToolStripMenuItem.Click += new System.EventHandler(this.likeSongToolStripMenuItem_Click);
+            // 
+            // artistsToolStripMenuItem
+            // 
+            this.artistsToolStripMenuItem.BackColor = System.Drawing.Color.PaleGreen;
+            this.artistsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.searchSongsForSpecificToolStripMenuItem});
+            this.artistsToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.artistsToolStripMenuItem.Name = "artistsToolStripMenuItem";
+            this.artistsToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.artistsToolStripMenuItem.Text = "Artists";
+            // 
+            // searchSongsForSpecificToolStripMenuItem
+            // 
+            this.searchSongsForSpecificToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchSongsForSpecificToolStripMenuItem.Name = "searchSongsForSpecificToolStripMenuItem";
+            this.searchSongsForSpecificToolStripMenuItem.Size = new System.Drawing.Size(237, 22);
+            this.searchSongsForSpecificToolStripMenuItem.Text = "Search Songs for Specific Artist";
+            this.searchSongsForSpecificToolStripMenuItem.Click += new System.EventHandler(this.searchSongsForSpecificToolStripMenuItem_Click);
+            // 
+            // myProfileToolStripMenuItem
+            // 
+            this.myProfileToolStripMenuItem.BackColor = System.Drawing.Color.PaleGreen;
+            this.myProfileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewLibraryToolStripMenuItem});
+            this.myProfileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myProfileToolStripMenuItem.Name = "myProfileToolStripMenuItem";
+            this.myProfileToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.myProfileToolStripMenuItem.Text = "My Profile";
+            this.myProfileToolStripMenuItem.Click += new System.EventHandler(this.myProfileToolStripMenuItem_Click);
+            // 
+            // viewLibraryToolStripMenuItem
+            // 
+            this.viewLibraryToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewLibraryToolStripMenuItem.Name = "viewLibraryToolStripMenuItem";
+            this.viewLibraryToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewLibraryToolStripMenuItem.Text = "View Library";
+            this.viewLibraryToolStripMenuItem.Click += new System.EventHandler(this.viewLibraryToolStripMenuItem_Click);
+            // 
+            // manageAccountToolStripMenuItem
+            // 
+            this.manageAccountToolStripMenuItem.BackColor = System.Drawing.Color.PaleGreen;
+            this.manageAccountToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editPackageToolStripMenuItem});
+            this.manageAccountToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.manageAccountToolStripMenuItem.Name = "manageAccountToolStripMenuItem";
+            this.manageAccountToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.manageAccountToolStripMenuItem.Text = "Manage Account";
+            // 
+            // editPackageToolStripMenuItem
+            // 
+            this.editPackageToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editPackageToolStripMenuItem.Name = "editPackageToolStripMenuItem";
+            this.editPackageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editPackageToolStripMenuItem.Text = "Edit Package";
+            this.editPackageToolStripMenuItem.Click += new System.EventHandler(this.editPackageToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome To Spotify";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.SpringGreen;
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(12, 388);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(79, 32);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.BackColor = System.Drawing.Color.SpringGreen;
+            this.logoutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutButton.Location = new System.Drawing.Point(549, 388);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(89, 32);
+            this.logoutButton.TabIndex = 3;
+            this.logoutButton.Text = "Logout";
+            this.logoutButton.UseVisualStyleBackColor = false;
+            this.logoutButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(660, 450);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "mainForm";
+            this.Text = "mainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem songsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem artistsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem myProfileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem browseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem byCategoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bySongIDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem likeSongToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchSongsForSpecificToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageAccountToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewLibraryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editPackageToolStripMenuItem;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button logoutButton;
+    }
+}
---
+++ pro/mainForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pro
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void myProfileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchSongsForSpecificToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SearchForm BF = new SearchForm();
+            this.Hide();
+            BF.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //logout
+        {
+            RegisterForm RF = new RegisterForm();
+            this.Hide();
+            RF.Show();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void byCategoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SearchForm BF = new SearchForm();
+            this.Hide();
+            BF.Show();
+        }
+
+        private void bySongIDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SearchForm BF = new SearchForm();
+            this.Hide();
+            BF.Show();
+        }
+
+        private void viewLibraryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //call yousra's report (but first: right click on the project name -> add -> existing item -> add only form.cs ale by-view al report bt3k + crystalReport.rpt)
+            /* EX:
+             *  FormName  XX = new FormName();       "FormName asm ale feha al report"
+             *  this.Hide();
+             *  XX.Show();
+             *  
+             *  PLEASE NOTE: 3'yry namespace bt3 al form.cs ale ht3mlelha include to (namespace pro) in both form.cs & form.designer.cs 
+             */
+        }
+        private void editPackageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //call omar's report (but first: right click on the project name -> add -> existing item -> add only form.cs)
+            /* EX:
+             *  FormName  XX = new FormName();       "FormName asm ale feha al report"
+             *  this.Hide();
+             *  XX.Show();
+             *  PLEASE NOTE, to solve l error : 3'yry namespace bt3 al form.cs ale ht3mlelha include to (namespace pro) in both form.cs & form.designer.cs 
+             */
+        }
+        private void likeSongToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             DisconnectedMode  XX = new DisconnectedMode();
+             this.Hide();
+             XX.Show();      
+        }
+    }
+}
